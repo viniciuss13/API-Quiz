d1f2f37a1aae610d8d85267d52bcb29e3431b9dc
gamarques <gabrielmarques.5269@aluno.saojudas.br>
2023-06-04 19:42:26 -0300

    criação de tabelas

--- index.js
+++ index.js
@@ -13,6 +13,51 @@ const connection = mysql.createConnection({
   database: 'db_quiz'
 });
 
+// Função para criar as tabelas
+function createTables() {
+  // Tabela 'questions'
+  const createQuestionsTableQuery = `
+    CREATE TABLE IF NOT EXISTS questions (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      question TEXT,
+      tags VARCHAR(255),
+      difficulty VARCHAR(255)
+    );
+  `;
+
+  // Tabela 'answers'
+  const createAnswersTableQuery = `
+    CREATE TABLE IF NOT EXISTS answers (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      question_id INT,
+      answer TEXT,
+      is_correct BOOLEAN,
+      FOREIGN KEY (question_id) REFERENCES questions(id)
+    );
+  `;
+
+  // Executando as consultas
+  connection.query(createQuestionsTableQuery, (error, results) => {
+    if (error) {
+      console.error('Erro ao criar a tabela "questions":', error);
+    } else {
+      console.log('Tabela "questions" criada com sucesso!');
+    }
+  });
+
+  connection.query(createAnswersTableQuery, (error, results) => {
+    if (error) {
+      console.error('Erro ao criar a tabela "answers":', error);
+    } else {
+      console.log('Tabela "answers" criada com sucesso!');
+    }
+  });
+}
+
+// Chamando a função para criar as tabelas
+createTables();
+
+
 // Conectar ao banco de dados
 connection.connect((err) => {
   if (err) {
