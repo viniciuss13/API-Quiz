b9376c82cd79051989f49bcfde2e1abd7ce51b9c
gamarques <gabrielmarques.5269@aluno.saojudas.br>
2023-06-04 21:22:10 -0300

    update

--- app.js
+++ app.js
@@ -38,29 +38,49 @@ app.get('/questoes/:id', (req, res) => {
 
 // Rota para criar uma nova questão
 app.post('/questoes', (req, res) => {
-  const questao = req.body;
+  const { pergunta, tag, dificuldade, resposta } = req.body;
+  const questao = { pergunta, tag, dificuldade };
+
   connection.query('INSERT INTO questoes SET ?', questao, (err, results) => {
     if (err) throw err;
-    res.json({ id: results.insertId });
+
+    const questaoId = results.insertId;
+    const respostaCorreta = { questao_id: questaoId, resposta, correta: true };
+
+    connection.query('INSERT INTO respostas SET ?', respostaCorreta, (err) => {
+      if (err) throw err;
+      res.json({ id: questaoId });
+    });
   });
 });
 
 // Rota para atualizar uma questão existente
 app.put('/questoes/:id', (req, res) => {
   const id = req.params.id;
-  const questao = req.body;
+  const { pergunta, tag, dificuldade, resposta } = req.body;
+  const questao = { pergunta, tag, dificuldade };
+
   connection.query('UPDATE questoes SET ? WHERE id = ?', [questao, id], (err) => {
     if (err) throw err;
-    res.sendStatus(200);
+
+    connection.query('UPDATE respostas SET resposta = ? WHERE questao_id = ?', [resposta, id], (err) => {
+      if (err) throw err;
+      res.sendStatus(200);
+    });
   });
 });
 
 // Rota para excluir uma questão
 app.delete('/questoes/:id', (req, res) => {
   const id = req.params.id;
-  connection.query('DELETE FROM questoes WHERE id = ?', id, (err) => {
+
+  connection.query('DELETE FROM respostas WHERE questao_id = ?', id, (err) => {
     if (err) throw err;
-    res.sendStatus(200);
+
+    connection.query('DELETE FROM questoes WHERE id = ?', id, (err) => {
+      if (err) throw err;
+      res.sendStatus(200);
+    });
   });
 });
 
